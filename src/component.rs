d76977ae8f38b35a68b95a9b3c5f3b9468c9e8b8
Nathan Stiles <stiles.nathan@outlook.com>
2025-03-08 03:14:31 -0500

    work

--- src/component.rs
+++ src/component.rs
@@ -1,44 +1,42 @@
 pub mod transform;
 
 use std::{
-    any::{Any, TypeId},
-    collections::HashMap
+    alloc::{self, Layout}, any::TypeId, collections::HashMap
 };
 
-const ENTITY_COUNT: usize = 1024;
+pub const ENTITY_COUNT: usize = 1024;
 pub struct Component {
-    component_arrays: HashMap<TypeId, Box<dyn Any + 'static>>,
+    component_arrays: HashMap<TypeId, *mut u8>,
 }
-
-
 
 impl Component {
     pub fn new() -> Component {
         Component {
-            component_arrays: HashMap::new()
+            component_arrays: HashMap::new(),
         }
     }
-    pub fn register<T>(&mut self, initial: T)
-    where
-        T: Copy + 'static,
-    {
-        let id = TypeId::of::<T>();
-        let thing = Box::new(vec![initial; ENTITY_COUNT]);
-        self.component_arrays.insert(id, thing);
-    }
-
     pub fn count(&self) -> usize {
         self.component_arrays.len()
     }
 
-    pub fn get<T>(self, index: usize) -> T
+    pub fn register<T>(&mut self)
     where
-        T: Copy + 'static,
+        T: 'static,
     {
-        let id = &TypeId::of::<T>();
-        let bx= self.component_arrays.get(id).unwrap();
-        let vec = bx.downcast_ref::<Vec<T>>().unwrap();
-        vec[index]
+        let id = TypeId::of::<T>();
+        let layout = Layout::array::<T>(ENTITY_COUNT).unwrap();
+        let pointer = unsafe { alloc::alloc_zeroed(layout) };
+        self.component_arrays.insert(id, pointer);
     }
 
+    pub fn get<T>(&self, index: usize) -> &mut T
+    where
+        T: 'static,
+    {
+        let id = &TypeId::of::<T>();
+        let pointer = self.component_arrays.get(id).unwrap();
+        let loc = unsafe { pointer.add(index * size_of::<T>()) };
+        let t = loc as *mut T;
+        unsafe{&mut * t}
+    }
 }
