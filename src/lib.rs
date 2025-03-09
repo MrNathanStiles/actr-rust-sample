561d46bf4e1e7714367cb8d4497c0d02bf41f2b6
Nathan Stiles <stiles.nathan@outlook.com>
2025-03-08 21:18:43 -0500

    broken

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 mod actr;
-mod component;
-mod data;
 mod state;
+mod ecs;
 
-use std::alloc::{self, Layout};
+use actr::{_actr_log_length, actr_performance};
 
-use actr::_actr_log_length;
-use component::{transform::Transform, ENTITY_COUNT};
 use state::State;
 
 #[unsafe(no_mangle)]
@@ -20,36 +17,41 @@ pub extern fn actr_init(state_pointer: *mut State, _w: f32, _h: f32) {
     //let state = unsafe { state_pointer.as_mut().unwrap() };
     let state = unsafe { state_pointer.as_mut().unwrap() };
     //let state = unsafe { &mut((((*state_pointer)))) };
-    state.component.register::<Transform>();
+    state.component_manager.register_component::<Transform>();
     let x = state.component.count();
     log(format!("init {x}"));
 
+    let q = ComponentArray;
 }
 
 #[unsafe(no_mangle)]
 pub extern fn actr_step(state_pointer: *mut State, _delta: f32) {
     let state = unsafe { state_pointer.as_mut().unwrap() };
     
-    let t = state.component.get::<Transform>(0);
 
-    t.position.x += 0.01;
-    t.position.z = 1.0;
+
+    let start = unsafe { actr_performance() };
+
+    for n in 0..MAX_ENTITIES {
+        let t = state.component.get::<Transform>(n);
+
+
+        t.position.x += 0.02;
+        t.position.z = n as f64;
+        
+        //let x = t.position.x;
+        //let z = t.position.z;
+        //let x = state.component.count();
+        //log(format!("0 step {x},{z}"));
+    }
+
+    let end = unsafe { actr_performance() };
+
+    let time = end - start;
+
+    log(format!("time {time}"));
+
     
-    let x = t.position.x;
-    let z = t.position.z;
-    //let x = state.component.count();
-    log(format!("0 step {x},{z}"));
-
-
-    let t = state.component.get::<Transform>(1);
-
-    t.position.x += 0.02;
-    t.position.z = 2.0;
-    
-    let x = t.position.x;
-    let z = t.position.z;
-    //let x = state.component.count();
-    log(format!("0 step {x},{z}"));
 }
 
 fn log(message: String) {
